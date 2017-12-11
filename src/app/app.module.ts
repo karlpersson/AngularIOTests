1080afd16452cbad5a21aa7916eac7a36b90ca54
Karl Persson <KP@ESS.LOCAL>
2017-12-11 10:51:10 +0100

    build fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { BannerInline } from './banner-inline.component/banner-inline.component.component';
 import { BannerInlineComponent } from './banner-inline/banner-inline.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BannerInline.ComponentComponent,
     BannerInlineComponent
   ],
   imports: [
